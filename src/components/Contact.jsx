549713d0ab4a62154e08ee5593f3c594a85c822d
unknown <tarrmmy@gmail.com>
2023-09-17 01:26:22 +0100

    Everything almost completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import { useState, useRef} from 'react'
+import { motion } from 'framer-motion'
+import emailjs from '@emailjs/browser'
+
+import { styles } from '../styles'
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from '../utils/motion'
+
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+
+    const formRef = useRef();
+    const [form, setForm] = useState ({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const [loading, setLoading] = useState(false)
+
+    const handleChange = (e) => {}
+
+    const handleSubmit = (e) => {}
+    return (
+        <div className='xl:m-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+            <motion.div
+            variants={slideIn('left', 'tween', 0.2, 1)}
+            className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact</h3>
+
+                <form 
+                    ref={formRef}
+                    onSubmit={handleSubmit}
+                    className='mt-12 flex flex-col'    
+                >
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>Your Name</span>
+                        <input 
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder='Input your name'
+                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>Your Email</span>
+                        <input 
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder='Input your email'
+                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+
+                    <label className='flex flex-col'>
+                        <span className='text-white font-medium mb-4'>Your Message</span>
+                        <textarea 
+                            rows='7'
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder='Input your thoughts'
+                            className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+                        />
+                    </label>
+
+                    <button 
+                        type='submit'
+                        className='font-bold shadow-md shadow-primary text-white py-3 px-8 mt-5 bg-tertiary outline-none w-fit rounded-xl'
+                    >
+                        {loading ? 'Sending...' : 'Send'}
+                    </button>
+                </form>
+            </motion.div>
+
+            <motion.div
+            variants={slideIn('right', 'tween', 0.2, 1)}
+            className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+            >
+                <EarthCanvas />
+            </motion.div>
+        </div>
+    )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Suspense, useState, useRef} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Point, PointMaterial, Preload } from '@react-three/drei'
 
-const Stars = () => {
+
+import *as random from 'maath/random/dist/maath-random.esm'
+
+const Stars = (props) => {
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array (5000), {radius: 1.2})
   return (
-    <div>Stars</div>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Point ref={ref} position={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial 
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Point>
+    </group>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className='z-[-1] inset-0 absolute h-auto w-full'>
+      <Canvas camera={{ position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all/>
+      </Canvas>
+    </div>
+  )
+}
+
+export default StarsCanvas
